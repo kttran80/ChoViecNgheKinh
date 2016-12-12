9d82840d54765c845ea9eda260bd51115a25c63c
kttran80 <tranthekiet@yahoo.com>
2016-12-11 23:59:39 -0800

    choviecnghekinh

---
+++ Program.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChoViecNgheKinh
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("test");
+        }
+    }
+}
